1d35908f06a58d5b46fc7f8a57b91e78bb9d8d47
TsabitFarel <tsabitzfarel@gmail.com>
2021-01-03 09:30:58 +0700

    Initial Commit

---
+++ FromUI/FromUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FromUIApp.swift
+//  FromUI
+//
+//  Created by Tsabit Farel on 03/01/21.
+//
+
+import SwiftUI
+
+@main
+struct FromUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
